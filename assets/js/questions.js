08f584bedcda0c668f78d6e2f82f0e4de771d2f0
Benny Trance <benjaminb.tran@gmail.com>
2021-05-31 17:26:29 -0500

    add a readme file. moved the questions array to its own js file. created a startQuiz and getNewQuestion function.

---
+++ assets/js/questions.js
@@ -0,0 +1,46 @@
+var questions = [
+  {
+    question: 'Which of the following is correct about features of JavaScript?',
+    choice1: 'JavaScript is a lightweight, interpreted programming language.',
+    choice2:
+      'JavaScript is designed for creating network-centric applications.',
+    choice3: 'JavaScript is complementary to and integrated with Java.',
+    choice4: 'All of the above.',
+    answer: 4,
+  },
+  {
+    question: 'Which of the following code creates an object?',
+    choice1: 'var book = Object();',
+    choice2: 'var book = new Object();',
+    choice3: 'var book = new OBJECT();',
+    choice4: 'var book = new Book();',
+    answer: 2,
+  },
+  {
+    question:
+      'Which of the following function of String object returns the character at the specified index?',
+    choice1: 'charAt()',
+    choice2: 'charCodeAt()',
+    choice3: 'concat()',
+    choice4: 'indexOf()',
+    answer: 1,
+  },
+  {
+    question:
+      'Which built-in method returns the index within the calling String object of the first occurrence of the specified value?',
+    choice1: 'getIndex()',
+    choice2: 'location()',
+    choice3: 'indexOf()',
+    choice4: 'None of the above.',
+    answer: 3,
+  },
+  {
+    question:
+      'Which of the following function of String object executes the search for a match between a regular expression and a specified string?',
+    choice1: 'concat()',
+    choice2: 'match()',
+    choice3: 'replace()',
+    choice4: 'search()',
+    answer: 4,
+  },
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,62 @@
 // WHEN the game is over
 // THEN I can save my initials and my score
 
+var question = document.querySelector('#question');
+var choices = Array.from(document.querySelectorAll('.choice-text'));
+
+var currentQuestion = {};
+var acceptingAnswers = false;
+var score = 0;
+var questionCounter = 0;
+var availableQuestions = [];
+var correctAnswer = 10;
+var maxQuestion = 5;
+
+function startQuiz() {
+  questionCounter = 0;
+  score = 0;
+  // copy the questions array to availableQustions array
+  availableQuestions = [...questions];
+  // make sure the array gets copied
+  //console.log(availableQuestions);
+  getNewQuestion();
+}
+
+startQuiz();
+
+function getNewQuestion() {
+  if (availableQuestions.length === 0 || questionCounter > maxQuestion) {
+    // go to the scores page
+    return window.location.assign('scores.html');
+  }
+  questionCounter++;
+  // get a random question from the availableQuestions array
+  var questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+  // add the choices to the question
+  choices.forEach((choice) => {
+    var number = choice.dataset['number'];
+    choice.innerText = currentQuestion['choice' + number];
+  });
+  // take out the qustion from the array after it has been used
+  availableQuestions.splice(questionIndex, 1);
+
+  acceptingAnswers = true;
+}
+
+choices.forEach((choice) => {
+  choice.addEventListener('click', (event) => {
+    if (!acceptingAnswers) {
+      return;
+    }
+    acceptingAnswers = false;
+    var selectedChoice = event.target;
+    // check to see if its choice is being clicked
+    // console.log(selectedChoice);
+    var selectedAnswer = selectedChoice.dataset['number'];
+    // check to see if its choice answer is being clicked
+    // console.log(selectedAnswer);
+    getNewQuestion();
+  });
+});
